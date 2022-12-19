05248062a4b7f5bab7e7339ab4c01264c6321ba2
vrcdrafter <102326953+vrcdrafter@users.noreply.github.com>
2022-12-19 14:11:52 -0500

    did some more work on fade script .

--- main_scene.gd
+++ main_scene.gd
@@ -4,13 +4,20 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+onready var current_texture  = get_node("picture1")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	print(current_texture.get_texture())
+	transition()
+	yield(get_tree().create_timer(1.0), "timeout")
+	
+	transition()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func transition():
+	$AnimationPlayer.play("New Anim")
