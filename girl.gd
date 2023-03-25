29aad432687d5a1215baa988ae23f3c2177b8fe8
vrcdrafter <102326953+vrcdrafter@users.noreply.github.com>
2023-03-25 09:15:15 -0400

    starting system for paining images

--- girl.gd
+++ girl.gd
@@ -5,12 +5,13 @@ extends Sprite3D
 # var a = 2
 # var b = "text"
 onready var complain = get_node("AudioStreamPlayer")
-	#
+onready var image_used = self.get_texture()
+onready var file_name = image_used.load_path
 onready var angry_text = preload("res://black_girl_angry.png")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	print("the picture is ",file_name)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -20,7 +21,7 @@ func _ready():
 func _on_Area_area_exited(area):
 	complain.play()
 	print("drink is gone ")
-
+	
 
 func _on_Area_area_entered(area):
 	print(area)
