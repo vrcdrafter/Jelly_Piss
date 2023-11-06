e3cd4db057d33fc5185a07abb22b926eb168c8ce
Mat Hawley <102326953+vrcdrafter@users.noreply.github.com>
2023-11-05 22:11:49 -0500

    added latina autoconvert.

--- girl.gd
+++ girl.gd
@@ -7,11 +7,21 @@ extends Sprite3D
 onready var complain = get_node("AudioStreamPlayer")
 onready var image_used = self.get_texture()
 onready var file_name = image_used.load_path
-onready var angry_text = preload("res://black_girl_angry.png")
+var angry_text 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("the picture is ",file_name)
-	
+	var regex = RegEx.new()
+	regex.compile("(beach)+([\\w\\s_-])+(.png)")
+	var result = regex.search(file_name)
+	if result != null:
+		var formated_string = result.get_string()
+		formated_string = formated_string.rstrip(".png")
+		print("regex match ", formated_string)
+		var angry_picture = "res://" + formated_string + "_angry.png"
+		print("added format string ", angry_picture)
+		angry_text = load(angry_picture)
+	print(result)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -20,7 +30,7 @@ func _ready():
 
 func _on_Area_area_exited(area):
 	complain.play()
-	print("drink is gone ")
+	#print("drink is gone ")
 	
 
 func _on_Area_area_entered(area):
@@ -28,3 +38,5 @@ func _on_Area_area_entered(area):
 	if(area.is_in_group("pee")):
 		print("Ive been peed on! ")
 		self.set_texture(angry_text)
+		print("tried to add", angry_text)
+		
